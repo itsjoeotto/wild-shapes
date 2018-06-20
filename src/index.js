74e6e39ce179451a59f86a19e067e075bb0155e8
Spengs <Jotto465@gmail.com>
2018-06-19 22:15:00 -0500

    implemented sass for learning purposes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './stylesheets/index.css';
 import Router from './components/Router';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
